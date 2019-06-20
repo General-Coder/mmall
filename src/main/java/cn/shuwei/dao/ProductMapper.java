8a8aa6a667d193fa1d557abcc9edf0b0500ac154
“zhangDing <17625904460@163.com>
2019-06-20 19:01:22 +0800

    mybatis逆向工程配置及生成

---
+++ src/main/java/cn/shuwei/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package cn.shuwei.dao;
+
+import cn.shuwei.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
---
+++ src/main/java/cn/shuwei/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package cn.shuwei.dao;
+
+import cn.shuwei.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
